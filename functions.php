58c95b5585b24eefe07802f12b0641adaf048036
Matt Rude <matt@mattrude.com>
2013-08-16 11:35:32 -0500

    Remove time from function mdr_new_blog_posts and add a title, see #1

--- functions.php
+++ functions.php
@@ -105,7 +105,6 @@ function mdr_restore_woo_stylesheet() {
 
 add_action( 'after_setup_theme', 'mdr_restore_woo_stylesheet' );
 
-
 /**
  * Blog posts on main page
  *
@@ -116,16 +115,17 @@ add_action( 'after_setup_theme', 'mdr_restore_woo_stylesheet' );
 function mdr_new_blog_posts() {
     if (have_posts()) { ?>
         <div id="home-blog" class="feature">
+            <h3 class="feature-title"><a href="/blog">Recent Posts</a></h3>
             <?php query_posts("posts_per_page=5");
             while (have_posts()) : the_post(); ?>
                 <li>
                     <?php $format = get_post_format(); ?>
                     <span class="icon-type-<?php echo $format; ?>"></span>
                     <a href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                    <time datetime="<?php the_time('c'); ?>" pubdate="pubdate"><?php the_date('Y M d'); ?></time>
                 </li>
             <?php endwhile; ?>
         </div>
     <?php }
 }
+
 ?>
