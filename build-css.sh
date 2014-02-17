335ce96f3d4274dbbc22c461a041b4fdc5d4ef1c
Matt Rude <matt@mattrude.com>
2014-02-17 13:14:43 -0600

    build-css.sh: correct location of yuicompressor

--- build-css.sh
+++ build-css.sh
@@ -7,6 +7,6 @@ sed "s/images/..\/..\/plugins\/jetpack\/modules\/sharedaddy\/images/g" ../../plu
 echo "div.sharedaddy .sd-content {width: 88%;}#content div.sharedaddy h3.sd-title {width:auto;}" >> style.css && \
 cat ../../plugins/jetpack/modules/widgets/widgets.css >> style.css && \
 rm -f style.css.gz && \
-java -jar /home/matt/bin/web/yuicompressor-2.4.8pre.jar style.css > style.min.css && \
+java -jar /root/bin/web/yuicompressor-2.4.8pre.jar style.css > style.min.css && \
 gzip style.min.css && \
 mv style.min.css.gz style.css.gz
